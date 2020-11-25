f0255761a3d0636ed7948054a4c5f72d600157cc
shakram02 <ahmedhamdyau@gmail.com>
2020-11-25 15:06:44 +0200

    support stream and one shot modes

--- reading_converter.py
+++ reading_converter.py
@@ -19,51 +19,78 @@ def twos(binary):
     return n
 
 
-axis_max = [0, 0, 0]
+def twos_bitwise(high, low):
+    out = 0
+    out |= high << 2
+    out |= ((low >> 6) & 0b11)
+    # 1100001111000000but
+    neg = out & (1 << 9)
 
-while True:
-    try:
-        line = sys.stdin.readline().strip()
-        line = [int(x) for x in digits.findall(line)]
+    if neg:
+        # 1101100010
+        # -158  <- expected
+        out = out ^ 0b1111111111    # Python has no bitwise negation
+        out = out + 1
+        out *= -1
 
-        if not line or len(line) != 6:
-            line = ""
-            print("skip")
-            time.sleep(0.3)
-            continue
+    return out
 
-        for i in range(0, len(line), 2):
-            high = line[i]
-            low = line[i+1]
 
-            num = "{:08b}{:08b}".format(high, low)
-            print(num, end=" ")
-            parsed_num = twos(num[:10])
+def read_stream():
+    axis_max = [0, 0, 0]
+    while True:
+        try:
+            line = sys.stdin.readline().strip()
+            # print(line)
+            line = [int(x) for x in digits.findall(line)]
 
-            axis_index = i // 2
-            axis_max[axis_index] = max(axis_max[axis_index], parsed_num)
+            if not line or len(line) != 6:
+                print("skip:", line)
+                # time.sleep(0.3)
+                continue
 
-            out = 0
-            out |= high << 8
-            out |= low
-            out >>= 6  # 1100001111000000
-            neg = out & (1 << 9)
+            for i in range(0, len(line), 2):
+                high = line[i]
+                low = line[i+1]
 
-            if neg:
-                # 1101100010
-                # 0866  <- out
-                # -158  <- expected
-                out = out ^ 0b1111111111    # Python has no bitwise negation
-                out = out + 1
-                out *= -1
-            print(f"[{out:04}]", end=" ")
-            print(f"{parsed_num:04}", end=" | ")
+                num = "{:08b}{:08b}".format(high, low)
+                print(num, end=" ")
+                parsed_num = twos(num[:10])
 
-        print()
+                axis_index = i // 2
+                axis_max[axis_index] = max(axis_max[axis_index], parsed_num)
 
-    except KeyboardInterrupt:
-        print("\n\nMAXS:", axis_max)
-        break
-    except Exception as e:
-        print("EXC", e)
-        exit(-1)
+                out = twos_bitwise(high, low)
+                print(f"[{out:04}]", end=" ")
+                print(f"{parsed_num:04}", end=" | ")
+
+            print()
+
+        except KeyboardInterrupt:
+            print("\n\nMAXS:", axis_max)
+            break
+        except Exception as e:
+            print("EXC", e)
+            exit(-1)
+
+
+def test_parse():
+    samples = [
+        ("1101110010", ["11011100", "10000000"]),
+        ("0000001001", ["00000010", "01000000"]),
+    ]
+    sample_index = 0
+    x = twos(samples[sample_index][0])
+    y = twos_bitwise(int(samples[sample_index][1][0], 2),
+                     int(samples[sample_index][1][1], 2)
+                     )
+    print(x, y)
+
+
+def main():
+    read_stream()
+    # test_parse()
+
+
+if __name__ == "__main__":
+    main()
